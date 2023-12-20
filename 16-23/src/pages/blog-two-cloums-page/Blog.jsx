53053b80625450040298b3f155ba48d1e17c286f
ademkaragulle <ademkaragulle@outlook.com>
2023-12-20 20:29:57 +0300

    add blog Iframe and clear database

--- 16-23/src/pages/blog-two-cloums-page/Blog.jsx
+++ 16-23/src/pages/blog-two-cloums-page/Blog.jsx
@@ -7,6 +7,7 @@ function Blog() {
         {
             "title": "The Difference Between Green Furniture and Sustainable Furniture",
             "img": "src/assets/img/blog/slider/one-1170x770.jpg",
+            "video": "",
             "shortText": "Many furniture companies claim their products are “green,” a designation usually referring to material sourcing. Maybe their sofa frames are made out of dumpster-sourced scrap metal or their cushions are…",
             "longText": [
                 "Organization: defined culturally as a key characteristic to success and easy living. The art of organization comes easy to some but may be a bit harder for others, especially those who are always busy and never seem to have enough hours in the day. Organization and productivity go hand in hand, each influencing the other. To keep levels of productivity high, you have to be organized and usually, organized individuals are productive. Fortunately, there are many different tactics you can use around your home to keep (at least) this sector of your life in order. Today we’re highlighting four products that make organization a breeze while saving space and maximizing time.",
@@ -33,7 +34,8 @@ function Blog() {
         },
         {
             "title": "A Busy Person Guide To Getting Organized Room",
-            "img": "src/assets/img/blog/slider/six-1170x770.jpg",
+            "img": "",
+            "video": "https://www.youtube.com/embed/0gCWPvRZcsI?si=GcFUvpArUl9nM3gj",
             "shortText": "Many furniture companies claim their products are “green,” a designation usually referring to material sourcing. Maybe their sofa frames are made out of dumpster-sourced scrap metal or their cushions are…",
             "longText": [
                 "Organization: defined culturally as a key characteristic to success and easy living. The art of organization comes easy to some but may be a bit harder for others, especially those who are always busy and never seem to have enough hours in the day. Organization and productivity go hand in hand, each influencing the other. To keep levels of productivity high, you have to be organized and usually, organized individuals are productive. Fortunately, there are many different tactics you can use around your home to keep (at least) this sector of your life in order. Today we’re highlighting four products that make organization a breeze while saving space and maximizing time.",
@@ -59,35 +61,9 @@ function Blog() {
             ]
         },
         {
-            "title": "Three Sneaky Storage Solutions For Small Spaces",
-            "img": "src/assets/img/blog/slider/two-1170x770.jpg",
-            "shortText": "Many furniture companies claim their products are “green,” a designation usually referring to material sourcing. Maybe their sofa frames are made out of dumpster-sourced scrap metal or their cushions are…",
-            "longText": [
-                "Organization: defined culturally as a key characteristic to success and easy living. The art of organization comes easy to some but may be a bit harder for others, especially those who are always busy and never seem to have enough hours in the day. Organization and productivity go hand in hand, each influencing the other. To keep levels of productivity high, you have to be organized and usually, organized individuals are productive. Fortunately, there are many different tactics you can use around your home to keep (at least) this sector of your life in order. Today we’re highlighting four products that make organization a breeze while saving space and maximizing time.",
-                "To be considered sustainable furniture, it should be high quality and support a lifestyle with reduced energy and resource needs. You can have a piece of furniture made of the most responsibly sourced materials out there, but if it falls apart in a short time and winds up in a landfill, or if it is designed in such a way that it can only fit into a carbon-spewing, resource-gobbling McMansion, it’s not sustainable. When it comes to sustainability, size does matter. Here’s why:",
-                "One of the best ways, if not the best way of reducing our carbon footprints and natural resource consumption is to reduce the physical footprints of our homes. Smaller homes produce less operational carbon emissions, (e.g., heating and cooling) than larger homes and are less resource intensive to build. Smaller homes tend to discourage excessive consumerism mitigating that behavior’s attendant environmental impact. Smaller homes typically support denser, more transit friendly communities, which, by their very nature, allow for a reduction in associated transit emissions. Location, Location, Location",
-                "Let’s face it, it’s the desire for – or perceived need for – more space that frequently drives people out of central, transit-friendly locations in search of more square footage. Since housing in most urban cores is considerably more expensive than suburban housing on a per square foot basis, moving to a larger home in a central location can be cost-prohibitive.",
-            ],
-            "slug": "Sneaky-Storage",
-            "author": "Admin",
-            "comments": [
-                {
-                    "userName": "bla bla",
-                    "comment": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, in."
-                },
-                {
-                    "userName": "bla bla",
-                    "comment": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, in."
-                },
-                {
-                    "userName": "bla bla",
-                    "comment": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, in."
-                },
-            ]
-        },
-        {
             "title": "The Future of Senior Housing",
-            "img": "src/assets/img/blog/slider/one-1170x770.jpg",
+            "img": "",
+            "video": "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/539079543&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true",
             "shortText": "Many furniture companies claim their products are “green,” a designation usually referring to material sourcing. Maybe their sofa frames are made out of dumpster-sourced scrap metal or their cushions are…",
             "longText": [
                 "Organization: defined culturally as a key characteristic to success and easy living. The art of organization comes easy to some but may be a bit harder for others, especially those who are always busy and never seem to have enough hours in the day. Organization and productivity go hand in hand, each influencing the other. To keep levels of productivity high, you have to be organized and usually, organized individuals are productive. Fortunately, there are many different tactics you can use around your home to keep (at least) this sector of your life in order. Today we’re highlighting four products that make organization a breeze while saving space and maximizing time.",
@@ -113,8 +89,37 @@ function Blog() {
             ]
         },
         {
+            "title": "Three Sneaky Storage Solutions For Small Spaces",
+            "img": "src/assets/img/blog/slider/two-1170x770.jpg",
+            "video": "",
+            "shortText": "Many furniture companies claim their products are “green,” a designation usually referring to material sourcing. Maybe their sofa frames are made out of dumpster-sourced scrap metal or their cushions are…",
+            "longText": [
+                "Organization: defined culturally as a key characteristic to success and easy living. The art of organization comes easy to some but may be a bit harder for others, especially those who are always busy and never seem to have enough hours in the day. Organization and productivity go hand in hand, each influencing the other. To keep levels of productivity high, you have to be organized and usually, organized individuals are productive. Fortunately, there are many different tactics you can use around your home to keep (at least) this sector of your life in order. Today we’re highlighting four products that make organization a breeze while saving space and maximizing time.",
+                "To be considered sustainable furniture, it should be high quality and support a lifestyle with reduced energy and resource needs. You can have a piece of furniture made of the most responsibly sourced materials out there, but if it falls apart in a short time and winds up in a landfill, or if it is designed in such a way that it can only fit into a carbon-spewing, resource-gobbling McMansion, it’s not sustainable. When it comes to sustainability, size does matter. Here’s why:",
+                "One of the best ways, if not the best way of reducing our carbon footprints and natural resource consumption is to reduce the physical footprints of our homes. Smaller homes produce less operational carbon emissions, (e.g., heating and cooling) than larger homes and are less resource intensive to build. Smaller homes tend to discourage excessive consumerism mitigating that behavior’s attendant environmental impact. Smaller homes typically support denser, more transit friendly communities, which, by their very nature, allow for a reduction in associated transit emissions. Location, Location, Location",
+                "Let’s face it, it’s the desire for – or perceived need for – more space that frequently drives people out of central, transit-friendly locations in search of more square footage. Since housing in most urban cores is considerably more expensive than suburban housing on a per square foot basis, moving to a larger home in a central location can be cost-prohibitive.",
+            ],
+            "slug": "Sneaky-Storage",
+            "author": "Admin",
+            "comments": [
+                {
+                    "userName": "bla bla",
+                    "comment": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, in."
+                },
+                {
+                    "userName": "bla bla",
+                    "comment": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, in."
+                },
+                {
+                    "userName": "bla bla",
+                    "comment": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, in."
+                },
+            ]
+        },
+        {
             "title": "Creating An Organized Multi-Use Room",
             "img": "src/assets/img/blog/slider/five-1170x770.jpg",
+            "video": "",
             "shortText": "Many furniture companies claim their products are “green,” a designation usually referring to material sourcing. Maybe their sofa frames are made out of dumpster-sourced scrap metal or their cushions are…",
             "longText": [
                 "Organization: defined culturally as a key characteristic to success and easy living. The art of organization comes easy to some but may be a bit harder for others, especially those who are always busy and never seem to have enough hours in the day. Organization and productivity go hand in hand, each influencing the other. To keep levels of productivity high, you have to be organized and usually, organized individuals are productive. Fortunately, there are many different tactics you can use around your home to keep (at least) this sector of your life in order. Today we’re highlighting four products that make organization a breeze while saving space and maximizing time.",
@@ -142,6 +147,7 @@ function Blog() {
         {
             "title": "Sustainable Furniture in Modern World",
             "img": "src/assets/img/blog/slider/six-1170x770.jpg",
+            "video": "",
             "shortText": "Many furniture companies claim their products are “green,” a designation usually referring to material sourcing. Maybe their sofa frames are made out of dumpster-sourced scrap metal or their cushions are…",
             "longText": [
                 "Organization: defined culturally as a key characteristic to success and easy living. The art of organization comes easy to some but may be a bit harder for others, especially those who are always busy and never seem to have enough hours in the day. Organization and productivity go hand in hand, each influencing the other. To keep levels of productivity high, you have to be organized and usually, organized individuals are productive. Fortunately, there are many different tactics you can use around your home to keep (at least) this sector of your life in order. Today we’re highlighting four products that make organization a breeze while saving space and maximizing time.",
@@ -167,11 +173,6 @@ function Blog() {
             ]
         }
     ]
-
-
-
-
-
 
 
     return (
@@ -250,10 +251,14 @@ function Blog() {
                                                     {/*=======  single post  =======*/}
                                                     <div className="single-blog-post">
                                                         <div className="single-blog-post__image">
-                                                            {blog.img &&
+                                                            {blog.img ?
                                                                 <Link to={`/blog-detail/${blog.slug}`}>
                                                                     <img src={blog.img} className="img-fluid" alt="" />
                                                                 </Link>
+                                                                :
+                                                                < div className="single-blog-post__image single-blog-post__image--audio ratio ratio-16x9">
+                                                                    <iframe width="560" height="315" src={blog.video} allowfullscreen></iframe>
+                                                                </div>
                                                             }
                                                         </div>
                                                         <div className="single-blog-post__content">
